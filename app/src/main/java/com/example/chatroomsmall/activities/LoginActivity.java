1a1832920889db612aaa8da1aa8a6b8ca8be8721
unknown <thaitrongtindev@gmail.com>
2023-09-21 14:07:33 +0700

    feature visibility password

--- app/src/main/java/com/example/chatroomsmall/activities/LoginActivity.java
+++ app/src/main/java/com/example/chatroomsmall/activities/LoginActivity.java
@@ -3,8 +3,10 @@ package com.example.chatroomsmall.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.chatroomsmall.R;
@@ -12,12 +14,36 @@ import com.example.chatroomsmall.R;
 public class LoginActivity extends AppCompatActivity {
 
     private Button btnLogin;
+    private EditText edtPassword;
+    private boolean isPasswordVisible = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         btnLogin = findViewById(R.id.btn_login);
+        edtPassword = findViewById(R.id.edt_password);
+        edtPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Xác định trạng thái hiện tại của mật khẩu và cập nhật nó
+                isPasswordVisible = !isPasswordVisible;
+                if (isPasswordVisible) {
+                    // Hiển thị mật khẩu
+                    edtPassword.setInputType(android.text.InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    edtPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.ic_visibility_off, 0);
+
+
+                } else {
+                    // Ẩn mật khẩu
+                    edtPassword.setInputType(android.text.InputType.TYPE_CLASS_TEXT | android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    edtPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.ic_eye, 0);
+                }
+            }
+        });
+
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
