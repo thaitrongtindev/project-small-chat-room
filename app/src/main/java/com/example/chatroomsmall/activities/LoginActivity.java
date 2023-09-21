fdc7cd66b2b3a58ab3813f647f090bd764d87b74
unknown <thaitrongtindev@gmail.com>
2023-09-21 13:42:57 +0700

    desin form form forgot  password

--- app/src/main/java/com/example/chatroomsmall/activities/LoginActivity.java
+++ app/src/main/java/com/example/chatroomsmall/activities/LoginActivity.java
@@ -3,14 +3,26 @@ package com.example.chatroomsmall.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.chatroomsmall.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, "TOAST", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
