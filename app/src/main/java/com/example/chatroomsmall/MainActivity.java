e1995aa23c31d0bd894836b0494f23961bc5f8b4
unknown <thaitrongtindev@gmail.com>
2023-09-22 12:22:54 +0700

    handle business logout

--- app/src/main/java/com/example/chatroomsmall/MainActivity.java
+++ app/src/main/java/com/example/chatroomsmall/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.chatroomsmall;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,12 +10,19 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         
         addControls();
         addEvents();
@@ -24,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addControls() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
     @Override
@@ -38,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.logout) {
             Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+            mAuth.signOut();
         }
         return super.onOptionsItemSelected(item);
 
