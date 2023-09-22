e0c466a249fb76708820e306ce7118b6c3b102d8
unknown <thaitrongtindev@gmail.com>
2023-09-22 12:27:46 +0700

    click enter move to main chat activtity

--- app/src/main/java/com/example/chatroomsmall/activities/StartChatActivity.java
+++ app/src/main/java/com/example/chatroomsmall/activities/StartChatActivity.java
@@ -1,16 +1,41 @@
 package com.example.chatroomsmall.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.chatroomsmall.MainActivity;
 import com.example.chatroomsmall.R;
 
 public class StartChatActivity extends AppCompatActivity {
 
+    private CardView cvEnter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        cvEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickEnter();
+            }
+        });
+    }
+
+    private void onClickEnter() {
+        startActivity(new Intent(StartChatActivity.this, MainActivity.class));
+        finish();
+    }
+
+    private void addControls() {
+        cvEnter = findViewById(R.id.cv_enter);
     }
 }
