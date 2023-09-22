33a2bbfac20c3ba07daf834bcf1cae60c4ef13e5
unknown <thaitrongtindev@gmail.com>
2023-09-22 09:42:30 +0700

    design startpage screen

---
+++ app/src/main/java/com/example/chatroomsmall/activities/StartChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.chatroomsmall.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chatroomsmall.R;
+
+public class StartChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
