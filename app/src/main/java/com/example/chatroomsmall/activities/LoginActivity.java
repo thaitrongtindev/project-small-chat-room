4520cc9a6c1d3b92854b513f6d95c8972c0e2968
unknown <thaitrongtindev@gmail.com>
2023-09-22 15:30:39 +0700

    handle business login google

--- app/src/main/java/com/example/chatroomsmall/activities/LoginActivity.java
+++ app/src/main/java/com/example/chatroomsmall/activities/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.example.chatroomsmall.activities;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,11 +18,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.chatroomsmall.MainActivity;
 import com.example.chatroomsmall.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -26,7 +40,11 @@ public class LoginActivity extends AppCompatActivity {
     private TextView tvRegister, tcForgotPassword;
     private boolean isPasswordVisible = false;
     private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
     private ProgressDialog progressDialog;
+
+    private GoogleSignInClient signInClient; // doi tuong nay dc sd
+    // tuong tac voi service google sign in. bao roi viec dang nhap
 
 
 
@@ -37,6 +55,19 @@ public class LoginActivity extends AppCompatActivity {
 
         addControls();
         addEvents();
+
+        // gui yeu cau xin phep login gooogle
+        createRequestLoginGoolge();
+
+    }
+
+    private void createRequestLoginGoolge() {
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(
+                GoogleSignInOptions.DEFAULT_SIGN_IN
+        ).requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        signInClient = GoogleSignIn.getClient(this, signInOptions);
 
     }
 
@@ -84,6 +115,7 @@ public class LoginActivity extends AppCompatActivity {
         btnLoginGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressDialog.show();
                 onClickLoginGoogle();
             }
         });
@@ -100,7 +132,58 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void onClickLoginGoogle() {
+        // open UI login google
+        Intent intent = signInClient.getSignInIntent();
+        activityResultLauncher.launch(intent);
     }
+
+    //get result retrun form login in form login
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data =result.getData();
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);// lấy data từ
+                        //thông tin đăng nhập từ intent data
+                        try {
+                            // lấy kết quả của việc đăng nhập
+                            GoogleSignInAccount account = task.getResult(ApiException.class);
+                            // có kết quả thì đem đi xac thức
+                            auth(account.getIdToken());// xác thực người dùng
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+
+    private void auth(String idToken) {
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(idToken, null);
+        // authcredential: đại diện thông tin xác thực từ người dùng
+        mAuth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                    userProfile();
+                } else {
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, "Login Fail", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void userProfile() {
+        mUser = mAuth.getCurrentUser();
+        if (mUser != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+
 
     private void onClickForgotPassword() {
         startActivity(new Intent(LoginActivity.this, ForgetPasswordActivity.class));
