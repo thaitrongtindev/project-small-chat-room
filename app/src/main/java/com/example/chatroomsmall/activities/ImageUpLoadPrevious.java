3523c0fe82410aea6f6f3e09fc17892f7e1e22ba
unknown <thaitrongtindev@gmail.com>
2023-09-27 21:55:01 +0700

    in activity imageUpload handle feature take photo again

--- app/src/main/java/com/example/chatroomsmall/activities/ImageUpLoadPrevious.java
+++ app/src/main/java/com/example/chatroomsmall/activities/ImageUpLoadPrevious.java
@@ -1,10 +1,18 @@
 package com.example.chatroomsmall.activities;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -13,17 +21,17 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ImageUpLoadPrevious extends AppCompatActivity {
 
-    
+
     private FloatingActionButton btnBack;
     private ImageView imageUpload;
     private ImageButton imgBtnCam, imBtnSend;
-    
-    
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_up_load_previous);
-        
+
         addControls();
         addEvents();
     }
@@ -34,14 +42,48 @@ public class ImageUpLoadPrevious extends AppCompatActivity {
         imgBtnCam = findViewById(R.id.imgbtn_camera_previous);
         imBtnSend = findViewById(R.id.imgbtn_send_previous);
 
-        Bundle  bundle = getIntent().getExtras();
-       // String image = bundle.getString("image_bitmap");
+        Bundle bundle = getIntent().getExtras();
+        // String image = bundle.getString("image_bitmap");
         Bitmap bitmap = getIntent().getParcelableExtra("image_bitmap");
-        Log.e("BITMAP", bitmap.toString() );
+        Log.e("BITMAP", bitmap.toString());
         imageUpload.setImageBitmap(bitmap);
     }
 
     private void addEvents() {
-        
+        imBtnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickSend();
+            }
+        });
+
+        imgBtnCam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickCamera();
+            }
+        });
+    }
+
+    private void onClickCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        activityResultLauncher.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Bundle data = result.getData().getExtras();
+                        Bitmap bitmap = (Bitmap) data.get("data");
+                        if (bitmap != null) {
+                            imageUpload.setImageBitmap(bitmap);
+                        }
+                    }
+                }
+            });
+
+    private void onClickSend() {
     }
 }
